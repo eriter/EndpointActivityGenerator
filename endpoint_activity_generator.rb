4fb637947ca3e3520773ffa54d00cdf2a7d60f1f
eriter <eli.riter@gmail.com>
2023-11-18 12:25:48 -0800

    log process start activity, assorted formatting, add an argument to the started process

--- endpoint_activity_generator.rb
+++ endpoint_activity_generator.rb
@@ -1,14 +1,39 @@
 require 'fileutils'
+require 'logger'
+require 'yaml'
 
 class EndpointActivityGeneration
     def initialize
-        @output_dir = 'activity_generation_log'
-        FileUtils.mkdir_p(@output_dir)
+      @output_dir = 'activity_generation_log'
+      FileUtils.mkdir_p(@output_dir)
+      @logger = Logger.new(File.join(@output_dir, 'activity_log.yml'))
     end
 
+    def log_activity(activity_type, details)
+      log_entry = {
+        'timestamp' => Time.now.utc,
+        'activity_type' => activity_type
+      }
+
+      log_entry.merge!(details)
+      @logger.info(log_entry.to_yaml)
+    end
+
+
     def start_process(executable_path, arguments = [])
-        command = "#{executable_path} #{arguments.join(' ')}"
-        process = IO.popen(command)
+      command = "#{executable_path} #{arguments.join(' ')}"
+      process_id = Process.spawn(command)
+
+
+      username = ENV['USER']
+      details = {
+        'username' => username,
+        'process_name' => File.basename(executable_path),
+        'process_command_line' => command,
+        'process_id' => process_id
+      }
+
+      log_activity('process_start', details)
     end
 end
 
@@ -16,4 +41,4 @@ PROCESS_TO_START = "/bin/ls"
 
 activity_generator = EndpointActivityGeneration.new
 
-activity_generator.start_process(PROCESS_TO_START)
+activity_generator.start_process(PROCESS_TO_START, ['-l'])
