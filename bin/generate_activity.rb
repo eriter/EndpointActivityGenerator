74b7bf9578a62fa67f1496a7a498af25f5a22bc1
eriter <eli.riter@gmail.com>
2023-11-25 11:58:23 -0800

    extract executable script and configuration constants to /bin

---
+++ bin/generate_activity.rb
@@ -0,0 +1,23 @@
+require_relative '../lib/activity_logger.rb'
+require_relative '../lib/endpoint_activity_generator.rb'
+require_relative '../lib/network_activity_generator.rb'
+require_relative '../lib/process_activity_generator.rb'
+
+PROCESS_TO_START = '/bin/ls'
+PROCESS_ARGS = ['-l']
+TEST_FILE_PATH = 'cromulent_doodle'
+OUTPUT_DIR = 'generated_activity'
+DESTINATION_ADDRESS = '8.8.8.8'
+DESTINATION_PORT = '53'
+NETWORK_DATA = 'Test message'
+
+activity_generator = EndpointActivityGenerator.new
+activity_logger = ActivityLogger.new(OUTPUT_DIR)
+network_activity = NetworkActivityGenerator.new
+process_activity = ProcessActivityGenerator.new
+
+process_activity.start_process(PROCESS_TO_START, PROCESS_ARGS, activity_logger)
+activity_generator.create_file(TEST_FILE_PATH, activity_logger)
+activity_generator.modify_file(TEST_FILE_PATH, activity_logger)
+activity_generator.delete_file(TEST_FILE_PATH, activity_logger)
+network_activity.generate_network_activity(DESTINATION_ADDRESS, DESTINATION_PORT, NETWORK_DATA, activity_logger)
