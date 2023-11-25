46bfdd5f3dfbbd90525ddaf57d02965c9b5779f9
eriter <eli.riter@gmail.com>
2023-11-25 12:41:15 -0800

    split out process start specs to their own file

--- spec/file_activity_generator_spec.rb
+++ spec/file_activity_generator_spec.rb
@@ -1,6 +1,5 @@
-require_relative '../lib/file_activity_generator.rb'
-require_relative '../lib/process_activity_generator.rb'
 require_relative '../lib/activity_logger.rb'
+require_relative '../lib/file_activity_generator.rb'
 require 'rspec'
 require 'tmpdir'
 
@@ -15,19 +14,6 @@ describe FileActivityGenerator do
 
   after(:each) do
     FileUtils.rm_rf(temp_dir)
-  end
-
-  describe '#start_process' do
-    before(:each) do
-      @process_activity = ProcessActivityGenerator.new
-      @process_activity.instance_variable_set(:@output_dir, temp_dir)
-    end
-
-    it 'creates a log file' do
-      @process_activity.start_process('/bin/ls', ['-l'], @logger)
-      log_file_path = File.join(temp_dir, 'activity_log.json')
-      expect(File.exist?(log_file_path)).to be true
-    end
   end
 
   describe '#create_file' do
---
+++ spec/process_activity_generator_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/activity_logger.rb'
+require_relative '../lib/process_activity_generator.rb'
+require 'rspec'
+require 'tmpdir'
+
+describe ProcessActivityGenerator do
+  let(:temp_dir) { Dir.mktmpdir }
+
+  before(:each) do
+    @logger = ActivityLogger.new(temp_dir)
+  end
+
+  after(:each) do
+    FileUtils.rm_rf(temp_dir)
+  end
+
+  describe '#start_process' do
+    before(:each) do
+      @process_activity = ProcessActivityGenerator.new
+      @process_activity.instance_variable_set(:@output_dir, temp_dir)
+    end
+
+    it 'creates a log file' do
+      @process_activity.start_process('/bin/ls', ['-l'], @logger)
+      log_file_path = File.join(temp_dir, 'activity_log.json')
+      expect(File.exist?(log_file_path)).to be true
+    end
+  end
+end
