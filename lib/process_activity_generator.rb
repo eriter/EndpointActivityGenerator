76b5cc73f046cc6a9739f2a96ca0960b87a50d87
eriter <eli.riter@gmail.com>
2023-11-26 15:19:14 -0800

    error handling for process creation (and execution?) to improve resilience

--- lib/process_activity_generator.rb
+++ lib/process_activity_generator.rb
@@ -1,17 +1,21 @@
 class ProcessActivityGenerator
 
   def start_process(executable_path, arguments = [], logger)
-    command = "#{executable_path} #{arguments.join(' ')}"
-    process = IO.popen(command)
+    begin
+      command = "#{executable_path} #{arguments.join(' ')}"
+      process = IO.popen(command)
 
-    activity_details = {
-      'process_name' => File.basename(executable_path),
-      'process_command_line' => command,
-      'process_id' => process.pid
-    }
+      activity_details = {
+        'process_name' => File.basename(executable_path),
+        'process_command_line' => command,
+        'process_id' => process.pid
+      }
 
-    logger.log_activity('process_start', activity_details)
-
-    process.close
+      logger.log_activity('process_start', activity_details)
+    rescue => e
+      logger.log_error("Error starting process: #{e.message}")
+    ensure
+      process&.close
+    end
   end
 end
