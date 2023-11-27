25259da1cb72a55f5da5aa60984d529ab9f0dc27
eriter <eli.riter@gmail.com>
2023-11-26 16:26:04 -0800

    break down start_process into modular methods

--- lib/process_activity_generator.rb
+++ lib/process_activity_generator.rb
@@ -2,20 +2,27 @@ class ProcessActivityGenerator
 
   def start_process(executable_path, arguments = [], logger)
     begin
-      command = "#{executable_path} #{arguments.join(' ')}"
-      process = IO.popen(command)
+      process_id = spawn_process(executable_path, arguments)
+      log_process_start(executable_path, arguments, process_id, logger)
+    rescue => e
+      logger.log_error("Error starting process: #{e.message}")
+    end
+  end
 
-      activity_details = {
+  private
+
+  def spawn_process(executable_path, arguments)
+    spawn(executable_path, *arguments)
+  end
+
+  def log_process_start(executable_path, arguments, process_id, logger)
+    activity_details = {
         'process_name' => File.basename(executable_path),
-        'process_command_line' => command,
-        'process_id' => process.pid
+        'process_command_line' => [executable_path, *arguments].join(' '),
+        'process_id' => process_id
       }
 
       logger.log_activity('process_start', activity_details)
-    rescue => e
-      logger.log_error("Error starting process: #{e.message}")
-    ensure
-      process&.close
     end
-  end
+
 end
